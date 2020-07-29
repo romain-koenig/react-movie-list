01a2862312e5f9517a14c89efca4ef5f1affddd7
Romain KOENIG <romain.koenig@gmail.com>
2020-07-29 22:14:56 +0200

    Adding PropTypes

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,8 +10,8 @@ const Card = ({ movie }) => {
         <h2>{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
       </div>
       <ul className="list-group list-group-flush">
-      <li className='list-group-item'>{`Distributor: ${movie.distributor}`}</li>
-      <li className='list-group-item'>{`Amount: ${movie.amount}`}</li>
+        <li className='list-group-item'>{`Distributor: ${movie.distributor}`}</li>
+        <li className='list-group-item'>{`Amount: ${movie.amount}`}</li>
       </ul>
     </div>
 
@@ -19,7 +19,18 @@ const Card = ({ movie }) => {
 };
 
 Card.propTypes = {
-  movie: PropTypes.object
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    distributor: PropTypes.string,
+    year: PropTypes.number,
+    amount: PropTypes.string,
+    img: PropTypes.shape({
+      src: PropTypes.string,
+      alt: PropTypes.string
+    }),
+    ranking: PropTypes.number
+  }).isRequired
+
 };
 
 export default Card;
