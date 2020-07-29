0dd4a0a98293722a6fbcaf15032ebff50bfe1bbd
Romain KOENIG <romain.koenig@gmail.com>
2020-07-29 15:52:57 +0200

    Just getting started and showing a title
    OK, react works, scripts are in place

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>movieList</h1>;
+  return <h1>This is the Movie List App</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
